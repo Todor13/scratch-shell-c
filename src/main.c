3eac880239846cf3f472e2ceafca2225fd909b4d
Todor Staykovski <todorstaykovski@gmail.com>
2026-02-08 09:12:42 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   setbuf(stdout, NULL);
   char *line = NULL;
   size_t len = 0;
   __ssize_t nread;
-  
-  printf("$ ");
-  nread = getline(&line, &len, stdin);
 
-  if (nread == -1) {
-    perror("getline");
-    free(line);
-    return 1;
+  while (1)
+  {
+    printf("$ ");
+    nread = getline(&line, &len, stdin);
+
+    if (nread == -1)
+    {
+      perror("getline");
+      free(line);
+      return 1;
+    }
+
+    if (nread > 0 && line[nread - 1] == '\n')
+      line[nread - 1] = '\0';
+
+    printf("%s: command not found\n", line);
   }
 
-  if (nread > 0 && line[nread - 1] == '\n')
-    line[nread - 1] = '\0';
-
-  printf("%s: command not found", line);
   free(line);
   return 0;
 }
