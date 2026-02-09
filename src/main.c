f861025ae81186b75617eec1dc4bad9e9c8613a9
Todor Staykovski <todorstaykovski@gmail.com>
2026-02-09 09:52:40 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+const int MAX_ARGS = 32;
+
+typedef int (*builtin_fn)(int argc, char **argv);
+
+struct builtin {
+  const char *name;
+  builtin_fn fn;
+};
+
+int builtin_echo(int argc, char **argv)
+{
+  for (int i = 1; i < argc; i++) {
+    printf("%s", argv[i]);
+    if (i + 1 < argc)
+      printf(" ");
+  }
+  printf("\n");
+  return 0;
+}
+
+struct builtin builtins[] = {
+  {"echo", builtin_echo},
+  {NULL, NULL}
+};
+
+int tokenize(char *line, char **argv) {
+  int argc = 0;
+  char *tok = strtok(line, " \t");
+  while(tok && argc < MAX_ARGS) {
+    argv[argc++] = tok;
+    tok = strtok(NULL, " \t");
+  }
+
+  argv[argc] = NULL;
+  return argc;
+}
+
 int main(int argc, char *argv[])
 {
   setbuf(stdout, NULL);
@@ -14,8 +51,7 @@ int main(int argc, char *argv[])
     printf("$ ");
     nread = getline(&line, &len, stdin);
 
-    if (nread == -1)
-    {
+    if (nread == -1) {
       perror("getline");
       free(line);
       return 1;
@@ -23,12 +59,29 @@ int main(int argc, char *argv[])
 
     if (nread > 0 && line[nread - 1] == '\n')
       line[nread - 1] = '\0';
-      
+
     if (strcmp(line, "exit") == 0) {
       free(line);
       return 0;
     }
-    printf("%s: command not found\n", line);
+
+    int result = -1;
+    char *argv[MAX_ARGS];
+    int argc = tokenize(line, argv);
+    for (int i = 0; builtins[i].name; i++) {
+      if (strcmp(argv[0], builtins[i].name) == 0) {
+        result = builtins[i].fn(argc, argv);
+        if (result != 0) {
+          free(line);
+          return result; 
+        }
+        break;
+      }
+    }
+    
+    if (result == -1) {
+      printf("%s: command not found\n", line);
+    }
   }
 
   free(line);
